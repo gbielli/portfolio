716b13dc961f6c084a468b4f1e6932baf4e1bbfa
gbielli <guillaume.bielli@gmail.com>
2023-09-30 23:10:14 +0200

    blog title error

--- app/components/Blog/BlogTitle/page.js
+++ app/components/Blog/BlogTitle/page.js
@@ -5,7 +5,7 @@ const BlogTitle = () => {
  <>
 <section className='pt-20 px-6'>
     <div className=' text-[12vw] leading-[10vw] font-clash'>Mes articles<span className='block'>& tutoriels</span></div>
-    <p className='text-2xl max-w-xl pt-10'>Passionné par le web depuis longtemps, J'explore et je vous partage mes expériences sur l'Acquisition, le Web Analytics et l'Automation.</p>
+    <p className='text-2xl max-w-xl pt-10'>Passionné par le web depuis longtemps, J&apos;explore et je vous partage mes expériences sur l&apos;Acquisition, le Web Analytics et l&apos;Automation.</p>
     <div className='h-[1px] bg-[#d4d4d4] w-full mt-20'></div>
     <div className='button text-black flex flex-wrap gap-5 mb-10 md:mb-10 mt-10'>
     <button className='flex gap-3 border border-black px-10 py-4 rounded-full items-center'>
